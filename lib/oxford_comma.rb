41c119e85bc8e62259bd2984b097839ef2b13aab
Luis Herrera <herrera.lrh@gmail.com>
2019-10-28 17:46:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length >= 3
-    array.to_sentence
+  elsif array.length == 3
+    array[-1].insert(0, "and ")
   end
 end
